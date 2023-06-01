8b318b453f805e3051f4cc196af59f9847d93b28
Hans Hoffman <hans@flatfile.io>
2023-06-01 11:11:11 -0600

    initial

---
+++ shell.nix
@@ -0,0 +1,14 @@
+let
+  sources = import ./nix/sources.nix { };
+  pkgs = import sources.nixpkgs { };
+in pkgs.mkShell {
+  buildInputs = [
+    pkgs.git
+    pkgs.nixfmt
+    pkgs.nodejs-18_x
+    pkgs.yarn
+    pkgs.purescript
+    pkgs.spago
+    pkgs.pkgs.nodePackages.purs-tidy
+  ];
+}
