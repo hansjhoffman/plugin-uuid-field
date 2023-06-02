f1961f95cc341c24e0f4073e2d9425c6c474d1b6
Hans Hoffman <hans@flatfile.io>
2023-06-02 08:20:05 -0600

    refactor using diff combinators and add tests

--- src/Main.purs
+++ src/Main.purs
@@ -1,22 +1,25 @@
-module Main (parse, UUID) where
+module Main (parse, UUID(..)) where
 
 import Prelude
 
-import Data.CodePoint.Unicode (isHexDigit)
+import Data.CodePoint.Unicode as Unicode
 import Data.Either (Either)
 import Data.String as Str
 import Parsing (Parser, ParseError)
 import Parsing as Parsing
 import Parsing.Combinators ((<?>))
-import Parsing.String (char, eof)
-import Parsing.String.Basic (takeWhile1)
+import Parsing.String as Parsing.String
+import Parsing.String.Basic as String.Basic
 import Control.Alt ((<|>))
 
 newtype UUID = UUID String
 
+derive instance eqUUID :: Eq UUID
+
 instance Show UUID where
   show (UUID val) = "(UUID '" <> val <> "')"
-  -- show (UUID val) = "(UUID 'uuid:" <> "v1" <> ":" <> val <> "')" -- uuid:v1:95ecc380-afe9-11e4-9b6c-751b66dd541e
+
+-- show (UUID val) = "(UUID 'uuid:" <> "v1" <> ":" <> val <> "')" -- uuid:v1:95ecc380-afe9-11e4-9b6c-751b66dd541e
 
 -- | Parse a possible uuid string.
 parse :: String -> Either ParseError UUID
@@ -29,26 +32,26 @@ parse input = do
 -- | https://www.ietf.org/rfc/rfc4122.txt
 parser :: Parser String UUID
 parser = do
-  chunk1 <- takeWhile1 isHexDigit -- time_low
-  _ <- char '-'
-  chunk2 <- takeWhile1 isHexDigit -- time_mid
-  _ <- char '-'
-  chunk3 <- takeWhile1 isHexDigit -- time_hi_and_version
-  _ <- char '-'
-  chunk4 <- takeWhile1 isHexDigit -- clock_seq
-  _ <- char '-'
-  chunk5 <- takeWhile1 isHexDigit -- node
-  eof <?> "end of string"
+  time_low <- String.Basic.takeWhile1 Unicode.isHexDigit
+  _ <- Parsing.String.char '-'
+  time_mid <- String.Basic.takeWhile1 Unicode.isHexDigit
+  _ <- Parsing.String.char '-'
+  time_hi_and_version <- String.Basic.takeWhile1 Unicode.isHexDigit
+  _ <- Parsing.String.char '-'
+  clock_seq <- String.Basic.takeWhile1 Unicode.isHexDigit
+  _ <- Parsing.String.char '-'
+  node <- String.Basic.takeWhile1 Unicode.isHexDigit
+  Parsing.String.eof <?> "end of string"
 
-  if (Str.length chunk1 /= 8) then
-    Parsing.fail "Expected chunk1 to be 8 hexadecimal digits"
-  else if (Str.length chunk2 /= 4) then
-    Parsing.fail "Expected chunk2 to be 4 hexadecimal digits"
-  else if (Str.length chunk3 /= 4) then
-    Parsing.fail "Expected chunk3 to be 4 hexadecimal digits"
-  else if (Str.length chunk4 /= 4) then
-    Parsing.fail "Expected chunk4 to be 4 hexadecimal digits"
-  else if (Str.length chunk5 /= 12) then
-    Parsing.fail "Expected chunk5 to be 12 hexadecimal digits"
+  if (Str.length time_low /= 8) then
+    Parsing.fail "Expected 1st chunk to be 8 hexadecimal digits"
+  else if (Str.length time_mid /= 4) then
+    Parsing.fail "Expected 2nd chunk to be 4 hexadecimal digits"
+  else if (Str.length time_hi_and_version /= 4) then
+    Parsing.fail "Expected 3rd chunk to be 4 hexadecimal digits"
+  else if (Str.length clock_seq /= 4) then
+    Parsing.fail "Expected 4th chunk to be 4 hexadecimal digits"
+  else if (Str.length node /= 12) then
+    Parsing.fail "Expected 5th chunk to be 12 hexadecimal digits"
   else
-    pure $ UUID (chunk1 <> "-" <> chunk2 <> "-" <> chunk3 <> "-" <> chunk4 <> "-" <> chunk5)
+    pure $ UUID (time_low <> "-" <> time_mid <> "-" <> time_hi_and_version <> "-" <> clock_seq <> "-" <> node)
--- test/Main.purs
+++ test/Main.purs
@@ -1,14 +1,36 @@
-module Test.Main where
+module Test.MainSpec where
 
 import Prelude
 
 import Data.Either (Either(..))
 import Effect (Effect)
-import Effect.Class.Console (log)
-import Main (parse, UUID)
+import Effect.Aff (launchAff_)
+import Main (parse, UUID(..))
+-- import Parsing (ParseError(..))
+import Test.Spec (describe, it, pending)
+import Test.Spec.Assertions (shouldEqual)
+import Test.Spec.Reporter.Console (consoleReporter)
+import Test.Spec.Runner (runSpec)
 
 main :: Effect Unit
-main = do
-  case parse "95ecc380-afe9-11e4-9b6c-751b66dd541e" of
-    Right uuid -> log $ show uuid
-    Left _ -> log "shit"
+main = launchAff_ $ runSpec [consoleReporter] do
+  describe "parse" do
+    it "should handle a valid uuid" do
+      let
+        actual = parse "95ecc380-afe9-11e4-9b6c-751b66dd541e"
+        expected = Right (UUID "95ecc380-afe9-11e4-9b6c-751b66dd541e")
+      actual `shouldEqual` expected
+
+    pending "should handle an invalid uuid (invalid chars)"
+
+    -- it "should handle an invalid uuid (too long)" do
+    --   let
+    --     actual = parse ("95ecc380-afe9-11e4-9b6c-751b66dd541e" <> "a")
+    --     expected = Left (ParseError "8" "Expected 5th chunk to be 12 hexadecimal digits")
+    --   actual `shouldEqual` expected
+
+    pending "should handle an invalid uuid (chunk1 too short)"
+    pending "should handle an invalid uuid (chunk2 too short)"
+    pending "should handle an invalid uuid (chunk3 too short)"
+    pending "should handle an invalid uuid (chunk4 too short)"
+    pending "should handle an invalid uuid (chunk5 too short)"
